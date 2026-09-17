dcca21b9664e7015b35fd42244a224a70bb70cec
Vinit <vinit.2007.20@gmail.com>
2026-09-17 23:53:53 +0530

    Refactor recursion.py to improve code structure and add missing newline; create day-08.md for recursion problems documentation

---
+++ Recursion/day-08.md
@@ -0,0 +1,6 @@
+#### Date - 17/09/2026
+
+# Problems on Recursion (Concepts)
+
+1. **Print Linearly from 1 to N**
+2. **Print Linearly from N to 1**
--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -1,5 +1,7 @@
-# 1 - Print name N times
 n = int(input())
+
+# 1 - Print name N times
+
 def printNTimes(name, count=0):
     if count>=n:
         return
@@ -7,4 +9,29 @@ def printNTimes(name, count=0):
     printNTimes(name, count + 1)
 printNTimes('vinit')
 
-# 2 -
+# 2 - Print Linearly from 1 to N
+
+def printTillN(count=1):
+    if count>n:
+        return
+    print(count)
+    printTillN(count+1)
+printTillN()
+
+# 3 - Print Linearly from N to 1
+
+def printTillN(count=n):
+    if count<1:
+        return
+    print(count)
+    printTillN(count-1)
+printTillN()
+print("done")
+# 4 - Backtracking
+
+def backtrack(count=n):
+    if count<1:
+        return
+    backtrack(count-1)
+    print(count)
+backtrack()
