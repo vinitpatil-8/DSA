40f6c7aa3770ae81b9d19c29efeab6df42d067cc
Vinit <vinit.2007.20@gmail.com>
2026-09-26 23:55:44 +0530

    optimal way of two sum

---
+++ Hashing/day-11.md
@@ -0,0 +1,5 @@
+#### Date - 26/09/2026
+
+# Hashing -
+<br>
+Basically a precalculation
--- LeetCode/Easy/two_sum.py
+++ LeetCode/Easy/two_sum.py
@@ -1,6 +1,9 @@
 nums = [2,7,11,15]
 target = int(input())
-for i, x in enumerate(nums):
-    for a, b in enumerate(nums):
-        if i != a and (x + b) == target:
-            print([i, a])
+seen = {}  # key: number, value: index
+
+for index, num in enumerate(nums):
+    complement = target - num
+    if complement in seen:
+        print([seen[complement], index])
+    seen[num] = index
